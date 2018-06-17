41e1a12985e7851292c57eff6be89ceafc86f97d
Andres Maza <andres.maza@barrelny.com>
2018-06-17 13:12:51 -0400

    Add remaining styling and content elements. Add ScrollMagic pinned nav

---
+++ js/main.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+  var veight;
+  var controller = new ScrollMagic.Controller();
+
+  var pinMainNav = new ScrollMagic.Scene({
+        triggerElement: '#main-nav',
+        triggerHook: 0
+    })
+    .setPin("#main-nav") // pins the element for the the scene's duration
+    .setClassToggle('#main-nav', 'is-fixed')
+    .addIndicators()
+    .addTo(controller); // assign the scene to the controller
+
+})
